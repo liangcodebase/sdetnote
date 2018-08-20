32f2174d616b359a511cbbb3699988a73d16b6fc
liangcodebase <38031698+liangcodebase@users.noreply.github.com>
2018-08-20 17:27:42 +0800

    Create python_practice.py

---
+++ python_practice.py
@@ -0,0 +1,13 @@
+# Print the prime number from 2 to 100
+
+def print_prime_number():
+    p = 2
+    while p <= 100:
+        is_prime = True
+        for i in range(2, p):
+            if p % i == 0:
+                is_prime = False
+                break
+        if is_prime == True:
+            print(p)
+        p = p + 1
